baf6101f67e98fe8e89d38ba23ea32b69eb69851
Parthiban Loganathan <pl2487@columbia.edu>
2014-08-02 23:50:17 -0700

    Add Phase 1

--- AwesomeApp/app/src/main/java/com/adi/awesomeapp/MainActivity.java
+++ AwesomeApp/app/src/main/java/com/adi/awesomeapp/MainActivity.java
@@ -1,16 +1,9 @@
 package com.adi.awesomeapp;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-
 
 
 public class MainActivity extends Activity {
@@ -19,11 +12,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
     }
 
 
@@ -44,21 +32,5 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
     }
 }
--- README.md
+++ README.md
@@ -9,4 +9,25 @@ Android Tutorial for ADI
 - Click Next on "Select the form factors your app will run on"
 - Choose Blank Activity with Fragment
 - Put `MainActivity` under Activity Name and click Next
--
+
+You now have an app with one Activity.
+
+Let's make it say a message
+
+Pre-requisites:
+- Knowledge of Java (1004, 1007)
+- Desire to learn
+- ~~Hatred for iOS~~
+
+Android Terminology:
+
+Views are anthing you can see on your screen.
+eg - ImageView, TextView, etc.
+
+Layouts are a means of grouping and organizing views.
+
+Activities are Java classes that loosely corresponds to a screen in your app.
+
+Broadcast receivers "catch" broadcasted messages and respond to them.
+
+Services are tasks that run in the background of your app.
